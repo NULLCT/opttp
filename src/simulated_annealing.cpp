2423d3b0f84ff4a79b37b22ffdedb9c6b2ddad59
NULLCT <cockatiel.u10@gmail.com>
2023-02-13 23:36:27 +0900

    より説明的な名前に

--- src/simulated_annealing.cpp
+++ src/simulated_annealing.cpp
@@ -4,15 +4,15 @@ double SA::frand() {
   return ((double)rand() / (RAND_MAX));
 }
 
-int64_t SA::evalScore(const STATE &state) {
+int64_t SA::evalScore(const STATE &_state) {
   int64_t res = 0;                          // 評価値
   std::set<std::tuple<int, int, int>> que;  // 時刻 番号 頂点
-  for (int i = 0; i < state.acthist.size(); i++) {
+  for (int i = 0; i < _state.acthist.size(); i++) {
     int time = 0;
-    for (int j = 0; j < state.acthist[i].size(); j++) {
-      que.insert({time, i, state.acthist[i][j]});
-      if (j + 1 != state.acthist[i].size())
-        time += model.DIST[state.acthist[i][j]][state.acthist[i][j + 1]];
+    for (int j = 0; j < _state.acthist[i].size(); j++) {
+      que.insert({time, i, _state.acthist[i][j]});
+      if (j + 1 != _state.acthist[i].size())
+        time += model.DIST[_state.acthist[i][j]][_state.acthist[i][j + 1]];
     }
   }
 
@@ -20,7 +20,7 @@ int64_t SA::evalScore(const STATE &state) {
   for (auto &[x, y] : model.Q)                     // 頂点xからyに行きたい
     m[x].insert(y);                                // 運送物登録
 
-  std::vector<std::set<int64_t>> carring(state.acthist.size());  // 各運送者が何を保持しているか
+  std::vector<std::set<int64_t>> carring(_state.acthist.size());  // 各運送者が何を保持しているか
 
   for (auto &q : que) {
     auto &[t, n, v] = q;  // 時刻 番号 頂点
@@ -36,21 +36,24 @@ int64_t SA::evalScore(const STATE &state) {
   return res;
 }
 
-void SA::modify(STATE &state) {
-  int carriernum = (rand() % state.acthist.size());                              // 何番目の運送者か
-  int posnum = (rand() % state.acthist[carriernum].size());                      // 運送経路履歴の何番目頂点を選ぶか
-  int outdnum = (rand() % model.G.g[state.acthist[carriernum][posnum]].size());  // 何本目の辺を選ぶか
-  int pos = state.acthist[carriernum][posnum];                                   // 運送者がいる頂点番号
-  int nextpos = model.G.g[pos][outdnum].to;                                      // これから行き来する頂点
+int64_t evalScoreFast(const STATE &_state_before_adaptation, const STATE &_state_after_adaptation) {
+}
 
-  state.acthist[carriernum].insert(state.acthist[carriernum].begin() + posnum + 1, nextpos);
-  state.acthist[carriernum].insert(state.acthist[carriernum].begin() + posnum + 2, pos);
+void SA::modify(STATE &_state) {
+  int carriernum = (rand() % _state.acthist.size());                              // 何番目の運送者か
+  int posnum = (rand() % _state.acthist[carriernum].size());                      // 運送経路履歴の何番目頂点を選ぶか
+  int outdnum = (rand() % model.G.g[_state.acthist[carriernum][posnum]].size());  // 何本目の辺を選ぶか
+  int pos = _state.acthist[carriernum][posnum];                                   // 運送者がいる頂点番号
+  int nextpos = model.G.g[pos][outdnum].to;                                       // これから行き来する頂点
 
-  state.pathlen[carriernum] += model.DIST[pos][nextpos] + model.DIST[nextpos][pos];
+  _state.acthist[carriernum].insert(_state.acthist[carriernum].begin() + posnum + 1, nextpos);
+  _state.acthist[carriernum].insert(_state.acthist[carriernum].begin() + posnum + 2, pos);
 
-  while (model.T < state.pathlen[carriernum]) {
-    state.pathlen[carriernum] -= model.DIST[state.acthist[carriernum].end()[-1]][state.acthist[carriernum].end()[-2]];
-    state.acthist[carriernum].pop_back();
+  _state.pathlen[carriernum] += model.DIST[pos][nextpos] + model.DIST[nextpos][pos];
+
+  while (model.T < _state.pathlen[carriernum]) {
+    _state.pathlen[carriernum] -= model.DIST[_state.acthist[carriernum].end()[-1]][_state.acthist[carriernum].end()[-2]];
+    _state.acthist[carriernum].pop_back();
   }
 }
 
@@ -59,7 +62,7 @@ STATE SA::simulated_annealing() {
   std::cerr << "init: " << initscore << "\n";
   int nonexped = 0;
   int exped = 0;
-  while (temp > 1) {  // 十分冷えるまで
+  while (temperature > 0.1) {
     for (int i = 0; i < repnum; i++) {
       STATE newstate = state;
       modify(newstate);
@@ -70,7 +73,7 @@ STATE SA::simulated_annealing() {
       if (0 < delta) {
         state = newstate;
         nonexped++;
-      } else if (frand() < std::exp(delta / temp)) {
+      } else if (frand() < std::exp(delta / temperature)) {
         state = newstate;
         exped++;
       }
@@ -80,8 +83,8 @@ STATE SA::simulated_annealing() {
         beststate = newstate;
       }
     }
-    temp *= coolingcoef;
-    std::cerr << "\e[2K\e[0E" << std::left << std::setw(8) << temp << " ("
+    temperature *= coolingcoef;
+    std::cerr << "\e[2K\e[0E" << std::left << std::setw(8) << temperature << " ("
               << "exp/nexp = " << exped << "/" << nonexped << ") " << beststate_score;
     nonexped = exped = 0;
   }
--- src/simulated_annealing.hpp
+++ src/simulated_annealing.hpp
@@ -14,7 +14,7 @@
 
 class SA {
   STATE state;               // 現在の状態
-  double temp;               // 温度
+  double temperature;               // 温度
   const int repnum;          // 反復回数
   const double coolingcoef;  // 冷却係数
   MODEL model;               // モデル
@@ -24,10 +24,11 @@ class SA {
   double frand();                         // [0,1]の乱数を返す
 
  public:
-  int64_t evalScore(const STATE &state);  // 評価関数
-  void modify(STATE &state);              // s-opt法 近傍に行き帰りするパスを追加
-  SA(STATE &_state, double _temp, int _repnum, double _coolingcoef, MODEL &_model) : state(_state),                      // 状態
-                                                                                     temp(_temp),                        // 温度
+  int64_t evalScore(const STATE &_state);  // 評価関数
+  int64_t evalScoreFast(const STATE &_state_before_adaptation, const STATE &_state_after_adaptation);  // 評価関数
+  void modify(STATE &_state);              // s-opt法 近傍に行き帰りするパスを追加
+  SA(STATE &_state, double _temperature, int _repnum, double _coolingcoef, MODEL &_model) : state(_state),                      // 状態
+                                                                                     temperature(_temperature),                        // 温度
                                                                                      repnum(_repnum),                    // 試行回数
                                                                                      coolingcoef(_coolingcoef),          // 冷却係数
                                                                                      model(_model),                      // 運送モデル
